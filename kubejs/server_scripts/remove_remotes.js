14f620c1bfaaf5dc078690bb492f76d0c4561176
Ramid Khan <ramidzkh@gmail.com>
2021-09-18 19:23:32 +1000

    Remove TF, make AE2 accessible

--- kubejs/server_scripts/remove_remotes.js
+++
@@ -1,9 +0,0 @@
-// Disabled as remotes are pretty balanced in skyblock
-// Remove remotes; they have infinite range for some reason
-false && onEvent("recipes", event => {
-    event.remove({ output: "storagenetwork:inventory_remote" });
-    event.remove({ output: "storagenetwork:crafting_remote" });
-    event.remove({ output: "storagenetwork:picker_remote" });
-    event.remove({ output: "storagenetwork:collector_remote" });
-    event.remove({ output: "storagenetwork:builder_remote" });
-});
--- kubejs/server_scripts/twilight_forest.js
+++
@@ -1,8 +0,0 @@
-const ACTIVATOR = "twilightforest:portal/activator";
-const TERRASTEEL = "#forge:ingots/terrasteel";
-
-// Makes the Twilight Forest portal activation more expensive
-onEvent('item.tags', event => {
-    event.removeAll(ACTIVATOR);
-    event.add(ACTIVATOR, TERRASTEEL);
-});
