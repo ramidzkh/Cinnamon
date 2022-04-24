a3b36b2645ec3c9796d469cf42e406150fda4142
Ramid Khan <ramidzkh@gmail.com>
2022-04-24 11:55:32 +1000

    Lead and Cobbled Deepslate

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -1,4 +1,6 @@
 onEvent('item.registry', event => {
+    event.create('cinnamon:mafic_minerals');
+
     event.create('cinnamon:the_rune_above_all').rarity('epic');
     event.create('cinnamon:incomplete_rune_above_all');
 });
